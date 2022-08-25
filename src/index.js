016b0657357416101acc439b3f47b4a16591d177
LArkhip <LArkhip@users.noreply.github.com>
2022-08-25 12:37:19 +0300

    API call for forecast

--- src/index.js
+++ src/index.js
@@ -62,6 +62,14 @@ function searchCity(event) {
   }
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "9cc1621f195afbca65aea792becaaa41";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
+}
+
 // displaying current City Weather
 function showTemperature(response) {
   console.log(response.data);
@@ -86,9 +94,11 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -153,5 +163,3 @@ function switchUnitsC(event) {
   //celcius.innerHTML = null;
 }
 celcius.addEventListener("click", switchUnitsC);
-
-displayForecast();
